05e5726337dfa64568262c4fe5e1380a1bbd4404
Yijiang Huang <yijiangh@mit.edu>
2022-09-28 14:21:56 +0200

    remove unused python modules

--- seq_dse/data_structures.py
+++
@@ -1,8 +0,0 @@
-from collections import namedtuple
-
-SeqElement = namedtuple('Element', ['name', 'fem_data', 'planning_data'])
-
-# used for FEM
-FEMData = namedtuple('FEMData', ['axis_points']) # , 'radius'
-# used for Pybullet planning
-PlanningData = namedtuple('PlanningData', ['start_pose', 'goal_pose', 'body', 'grasp_offsets'])
